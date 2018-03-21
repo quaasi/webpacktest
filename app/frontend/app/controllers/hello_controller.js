bbc6b1f98a7e8af1d04b286d52b2b1b69d97d601
Michael Quaas <michael@qua.as>
2018-03-21 11:33:01 +0100

    rename app/javascript -> app/frontend

--- app/frontend/app/controllers/hello_controller.js
+++ app/frontend/app/controllers/hello_controller.js
@@ -9,10 +9,11 @@
 
 import { Controller } from "stimulus"
 
-export default class extends Controller {
+class HelloController extends Controller {
   static targets = [ "name", "output" ];
 
   greet() {
     this.outputTarget.textContent = `Hello, ${this.nameTarget.value}!`
   }
 }
+export default HelloController
--- app/frontend/app/index.js
+++ app/frontend/app/index.js
@@ -6,7 +6,6 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 Rails.start();
 Turbolinks.start();
 
-
 const application = Application.start();
-const context = require.context("controllers", true, /.js$/);
+const context = require.context("./controllers", true, /.js$/);
 application.load(definitionsFromContext(context));
